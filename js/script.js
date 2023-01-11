e9efd5b647ed891844c494ee11d4ab481ccf9d69
nobrayn <nobrayn@gmail.com>
2023-01-11 18:41:02 -0500

    counter and image swap works, some namespacing, added reset function, added explosion WIP

--- js/script.js
+++ js/script.js
@@ -1,51 +1,41 @@
-// Create a namespace object to hold the app:
+// Creating a namespace object to hold the app
 const insultApp = {};
+
+// NAMESPACE THIS YO
 let isLeftSide = true; // variable to determine which side will call API/fill text bubble
-let leftCounter = 4;
-let rightCounter = 4; 
+let leftCounter = 5;
+let rightCounter = 5; 
 
-//figuring out the picture swapping function on the happy and angry side//
-  //make an array
-const leftPictures = new Array();
-leftPictures[0] = new Image();
-leftPictures[0].src = './photos/personLeft/personInsultLeft4.png';
-leftPictures[1] = new Image();
-leftPictures[1].src = './photos/personLeft/personInsultLeft3.png';
-leftPictures[2] = new Image();
-leftPictures[2].src = './photos/personLeft/personInsultLeft2.png';
-leftPictures[3] = new Image();
-leftPictures[3].src = './photos/personLeft/personInsultLeft1.png';
-leftPictures[4] = new Image();
-leftPictures[4].src = './photos/personLeft/personAILeft.png';
-leftPictures[5] = new Image();
-leftPictures[5].src = './photos/personLeft/personAdviceLeft4.png';
-leftPictures[6] = new Image();
-leftPictures[6].src = './photos/personLeft/personAdviceLeft3.png';
-leftPictures[7] = new Image();
-leftPictures[7].src = './photos/personLeft/personAdviceLeft2.png';
-leftPictures[8] = new Image();
-leftPictures[8].src = './photos/personLeft/personAdviceLeft1.png';
+insultApp.leftImage = document.getElementById('leftPersonImage');
+insultApp.rightImage = document.getElementById('rightPersonImage');
 
-const rightPictures = new Array();
-rightPictures[0] = new Image();
-rightPictures[0].src = './photos/personRight/personInsultright4.png';
-rightPictures[1] = new Image();
-rightPictures[1].src = './photos/personRight/personInsultRight3.png';
-rightPictures[2] = new Image();
-rightPictures[2].src = './photos/personRight/personInsultRight2.png';
-rightPictures[3] = new Image();
-rightPictures[3].src = './photos/personRight/personInsultRight1.png';
-rightPictures[4] = new Image();
-rightPictures[4].src = './photos/personRight/personAIRight.png';
-rightPictures[5] = new Image();
-rightPictures[5].src = './photos/personRight/personAdviceRight4.png';
-rightPictures[6] = new Image();
-rightPictures[6].src = './photos/personRight/personAdviceRight3.png';
-rightPictures[7] = new Image();
-rightPictures[7].src = './photos/personRight/personAdviceRight2.png';
-rightPictures[8] = new Image();
-rightPictures[8].src = './photos/personRight/personAdviceRight1.png';
-
+const leftPictures = [
+  './photos/explosion.png',
+  './photos/personLeft/personInsultLeft4.png',
+  './photos/personLeft/personInsultLeft3.png',
+  './photos/personLeft/personInsultLeft2.png',
+  './photos/personLeft/personInsultLeft1.png',
+  './photos/personLeft/personAILeft.png',
+  './photos/personLeft/personAdviceLeft4.png',
+  './photos/personLeft/personAdviceLeft3.png',
+  './photos/personLeft/personAdviceLeft2.png',
+  './photos/personLeft/personAdviceLeft1.png',
+  './photos/explosion.png',
+]
+// make array for right character image locations
+const rightPictures = [
+  './photos/explosion.png',
+  './photos/personRight/personInsultRight4.png',
+  './photos/personRight/personInsultRight3.png',
+  './photos/personRight/personInsultRight2.png',
+  './photos/personRight/personInsultRight1.png',
+  './photos/personRight/personAIRight.png',
+  './photos/personRight/personAdviceRight4.png',
+  './photos/personRight/personAdviceRight3.png',
+  './photos/personRight/personAdviceRight2.png',
+  './photos/personRight/personAdviceRight1.png',
+  './photos/explosion.png',
+]
 
 insultApp.getInsultLeft = () => {
   fetch('https://proxy-ugwolsldnq-uc.a.run.app/https://evilinsult.com/generate_insult.php?lang=en&type=jsonfetch(’https://proxy-ugwolsldnq-uc.a.run.app/https://evilinsult.com/generate_insult.php?lang=en&type=json&version='+Math.floor(Math.random()*100000+1))
@@ -54,7 +44,7 @@ insultApp.getInsultLeft = () => {
   })
   .then(function (jsonResult) {
     const insultLeftText = document.querySelector('#leftPersonSpeechText');
-    // clear existing content
+    // replace existing content
     insultLeftText.innerHTML = jsonResult.insult;
   })
 };
@@ -66,7 +56,7 @@ insultApp.getInsultRight = () => {
   })
   .then(function (jsonResult) {
     const insultRightText = document.querySelector('#rightPersonSpeechText');
-    // clear existing content
+    // replace existing content
     insultRightText.innerHTML = jsonResult.insult;
   });
 };
@@ -79,7 +69,7 @@ insultApp.getAdviceLeft = () => {
   .then(function (jsonResult) { 
     const advice = jsonResult.slip;
     const adviceLeftText = document.querySelector('#leftPersonSpeechText');
-    // clear existing content
+    // replace existing content
     adviceLeftText.innerHTML = advice.advice;
   });
 };
@@ -92,68 +82,87 @@ insultApp.getAdviceRight = () => {
   .then(function (jsonResult) {
     const advice = jsonResult.slip;
     const adviceRightText = document.querySelector('#rightPersonSpeechText');
-    // clear existing content
+    // replace existing content
     adviceRightText.innerHTML = advice.advice;
   });
 };
-//FOR COUNTERS
-//player 1
-    //insult button is clicked, the right side counter goes down by 1
-    //then retrieves right side photo
-    
-//player 2
-    //advice button is clicked, the left side counter goes up by 1
-    //then retrives left side photo
-    
-    insultApp.leftCounter = () => {
-      
-    }
 
-  // add event listeners
-  //insult button conditions
-    document.getElementById("insultButton").addEventListener("click", e => {
-      console.log('counter before any work is done: '+ leftCounter);
-    if (isLeftSide) {
-      insultApp.getInsultLeft();
+// NAMESPACE THIS YO
+const leftPersonTextReset = () => {
+  const reset = document.querySelector('#leftPersonSpeechText')
+  reset.innerHTML = '';
+}
 
-      //stuff for the counter
-      // rightCounter--;
-      // document.getElementById('rightPersonImage').src = rightPictures[rightCounter];
+const rightPersonTextReset = () => {
+  const reset = document.querySelector('#rightPersonSpeechText')
+  reset.innerHTML = '';
+}
 
-      console.log(isLeftSide);
-      isLeftSide = !isLeftSide;
+insultApp.gameReset = () => {
+  // images reset to neutral
+  insultApp.leftImage.src = leftPictures[4];
+  insultApp.rightImage.src = rightPictures[4];
+  // speech bubbles clear
+  leftPersonTextReset();
+  rightPersonTextReset();
+  leftCounter=  5;
+  rightCounter = 5;
+}
+
+// add event listeners
+// NAMESPACE THIS YO
+document.getElementById("insultButton").addEventListener("click", e => {
+  if (leftCounter === 0 ||
+    rightCounter === 0) {
+      alert(`You insulted them to death.`); // call function to reset game
+      insultApp.gameReset();
+      //player 1 (Left)
     } else {
-      insultApp.getInsultRight();
-      leftCounter--;
-      console.log(isLeftSide);
-      isLeftSide = !isLeftSide;
-      console.log('counter after decreasing:'+ leftCounter);
-    }
-
-
+      if (isLeftSide) {
+        insultApp.getInsultLeft();
+        rightCounter--;
+        isLeftSide = !isLeftSide;
+        insultApp.rightImage.src = rightPictures[rightCounter];
+        //player 2 (Right)
+        } else {
+          insultApp.getInsultRight();
+          leftCounter--;
+          isLeftSide = !isLeftSide;
+          insultApp.leftImage.src = leftPictures[leftCounter];
+      };
+    };
   });
+
   //advice button conditions
+    // NAMESPACE THIS YO
   document.getElementById("adviceButton").addEventListener("click", e => {
-    if (isLeftSide) {
-      insultApp.getAdviceLeft();
-      rightCounter++;
-      console.log(isLeftSide);
-      isLeftSide = !isLeftSide;
-    } else {
-      insultApp.getAdviceRight();
-      leftCounter++;
-      console.log(isLeftSide);
-      isLeftSide = !isLeftSide;
-    }
+      if (leftCounter === 10 ||
+        rightCounter === 10) {
+        alert(`You praised them into a new dimension!`); // call function to reset game
+        insultApp.gameReset();
+      //player 1 (Left)
+      } else {
+        if (isLeftSide) {
+          insultApp.getAdviceLeft();
+          rightCounter++;
+          isLeftSide = !isLeftSide;
+          insultApp.rightImage.src = rightPictures[rightCounter];
+        //player 2 (Right)
+        } else {
+          insultApp.getAdviceRight();
+          leftCounter++;
+          isLeftSide = !isLeftSide;
+          insultApp.leftImage.src = leftPictures[leftCounter];
+      };
+    };
   });
 
 // init function to kick off the code
 insultApp.init = () => {
-  let leftCounter = 4;
-  let rightCounter = 4; 
-  console.log(leftCounter);
-  console.log(rightCounter);
-  // checkout the stuff below, it is broken!
-  // document.getElementById('#leftPersonImage').src = leftPictures[leftCounter];
-  // document.getElementById('#rightPersonImage').src = rightPictures[rightCounter];  
+  insultApp.leftImage.src = leftPictures[leftCounter];
+  insultApp.rightImage.src = rightPictures[rightCounter];
 }
+
+insultApp.init();
+
+
